00ad0e6b7c38c87dc82144f9cd46eaea30395a96
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-18 19:13:07 +0200

    scaffold wordpress functional suite

---
+++ tests/wordpressfunctional/BasicNavigationCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class BasicNavigationCest
+{
+    public function _before(WordpressfunctionalTester $I)
+    {
+    }
+
+    public function _after(WordpressfunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(WordpressfunctionalTester $I)
+    {
+    }
+}
